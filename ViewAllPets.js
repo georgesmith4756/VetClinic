7e297ca192d2bd7ba34ede25fd5f1d1f483fca67
mltomlins0n <martin070891@gmail.com>
2019-09-06 15:12:24 +0100

    feature working

--- ViewAllPets.js
+++ ViewAllPets.js
@@ -1,17 +1,21 @@
 let getElements = (request) => {
-    let info = request.response;
-    formEL = getElementsByID("FormEL");
-    for(let element of formEl.elements) {
-        if(element.id) {
-            formData[element.id] = element.value;
-            formData[element.pets] = element.pets;
+    let info = JSON.parse(request.response);
+    let list = document.getElementById("listOfPets");
+    for(let owner of info) {
+        for (let pet of owner.pets){
+            let firstName = owner.firstName;
+            let name = pet.name;
+            let birthDate = pet.birthDate;
+            bulletPoint = document.createElement("li")
+            bulletPoint.innerText = "Owner: " + firstName + " Pet: " + name + " Pet's birthdate: " + birthDate;
+            list.appendChild(bulletPoint);
         }
     }
 }
 
-function viewPets(id){
+function viewPets(){
     let method = "GET";
-    let url = "http://localhost:9966/petclinic/api/owners/" + id;
+    let url = "http://localhost:9966/petclinic/api/owners/" ;
     let callback = getElements;
     let headers = {
         "Accept": "application/json;charset=UTF-8"
