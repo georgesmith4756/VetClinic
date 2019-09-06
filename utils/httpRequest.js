22445ec85fde23198b19bc9878a2819006df4460
--replace-all <aaron.glendinning@academytrainee.com>
2019-09-06 12:58:44 +0100

    import/export deleted

--- utils/httpRequest.js
+++ utils/httpRequest.js
@@ -1,12 +1,11 @@
-export default function httpRequest(method, url, callback, headers, body) {
+function httpRequest(method, url, callback, headers, body) {
     let request = new XMLHttpRequest();
     request.open(method, url);
-    request.onload(() => {
-        callback(request.response);
-    })
-
+    request.onload = () => {
+        callback(request);
+    }
     for (key in headers) {
-        request.setRequestHeader(key, headers.key);
+        request.setRequestHeader(key, headers[key]);
     }
     body ? request.send(body) : request.send();
 }
