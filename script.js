cce6b345d4433939c593f491fa74d00f098d69d1
georgesmith4756 <georgesmith4756@hotmail.co.uk>
2019-09-06 11:32:07 +0100

    added owner list

---
+++ script.js
@@ -0,0 +1,67 @@
+function getOwners(){
+const app = document.getElementById('root')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'http://localhost:9966/petclinic/api/owners', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  const card = document.createElement('div')
+
+    const h1 = document.createElement('h1')
+    h1.textContent = "List of Owners: ";
+    card.appendChild(h1);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(owner => {
+      
+      card.setAttribute('class', 'card');
+
+      const bootDiv = document.createElement('div');
+      bootDiv.className = "container";
+      bootDiv.style = "border:1px solid #cecece;";
+
+      const id = document.createElement('h4');
+      id.textContent = "ID: " + owner.id;
+
+      const firstName = document.createElement('h4');
+      firstName.textContent = "First Name: " + owner.firstName;
+
+      const lastName = document.createElement('h4');
+      lastName.textContent = "Last Name: " + owner.lastName;
+
+      const address = document.createElement('h4');
+      address.textContent = "Address: " + owner.address;
+
+      const city = document.createElement('h4');
+      city.textContent = "City: " + owner.city;
+
+      const telephone = document.createElement('h4');
+      telephone.textContent = "Telephone: " + owner.telephone;
+      
+     
+      
+      bootDiv.appendChild(id);
+      bootDiv.appendChild(firstName);
+      bootDiv.appendChild(lastName);
+      bootDiv.appendChild(address);
+      bootDiv.appendChild(city);
+      bootDiv.appendChild(telephone);
+
+
+      container.appendChild(bootDiv);
+     
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
+}
