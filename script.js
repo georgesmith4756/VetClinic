d12e3cb11fd25da1fbfe5f99be90bc31c55d24fb
georgesmith4756 <georgesmith4756@hotmail.co.uk>
2019-09-06 14:25:17 +0100

    added new code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getOwners(){
-const app = document.getElementById('root')
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+const app = document.getElementById('root');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
 app.appendChild(container)
 
@@ -9,8 +9,8 @@ var request = new XMLHttpRequest()
 request.open('GET', 'http://localhost:9966/petclinic/api/owners', true)
 request.onload = function() {
   // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  const card = document.createElement('div')
+  var data = JSON.parse(this.response);
+  const card = document.createElement('div');
 
     const h1 = document.createElement('h1')
     h1.textContent = "List of Owners: ";
@@ -18,6 +18,8 @@ request.onload = function() {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach(owner => {
+
+
       
       card.setAttribute('class', 'card');
 
@@ -57,11 +59,107 @@ request.onload = function() {
      
     })
   } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `it's not working`;
+    app.appendChild(errorMessage);
   }
 }
 
-request.send()
+request.send();
 }
+
+function viewSingleOwner(){
+
+const app = document.getElementById('root');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container)
+
+const input = document.createElement("input");
+const button2 = document.createElement("button");
+button2.innerHTML = "Submit";
+button2.addEventListener("click", getSingleUser(input.value));
+
+container.appendChild(input);
+container.appendChild(button2);
+
+}
+
+function getSingleUser(value){
+
+    alert("Hello");
+
+    var request = new XMLHttpRequest()
+    request.open('GET', 'http://localhost:9966/petclinic/api/owners', true)
+
+    request.onload = function() {
+    var data = JSON.parse(this.response);
+
+    const h1 = document.createElement('h1');
+    h1.textContent = "Viewing Single Owner: ";
+    
+    const app = document.getElementById('root');
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    const card = document.createElement('div');
+    card.appendChild(h1);
+
+  
+  
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach(owner => {
+        
+        if(owner.id = value){
+        card.setAttribute('class', 'card');
+  
+        const bootDiv = document.createElement('div');
+        bootDiv.className = "container";
+        bootDiv.style = "border:1px solid #cecece;";
+  
+        const id = document.createElement('h4');
+        id.textContent = "ID: " + owner.id;
+  
+        const firstName = document.createElement('h4');
+        firstName.textContent = "First Name: " + owner.firstName;
+  
+        const lastName = document.createElement('h4');
+        lastName.textContent = "Last Name: " + owner.lastName;
+  
+        const address = document.createElement('h4');
+        address.textContent = "Address: " + owner.address;
+  
+        const city = document.createElement('h4');
+        city.textContent = "City: " + owner.city;
+  
+        const telephone = document.createElement('h4');
+        telephone.textContent = "Telephone: " + owner.telephone;
+        
+       
+  
+        bootDiv.appendChild(id);
+        bootDiv.appendChild(firstName);
+        bootDiv.appendChild(lastName);
+        bootDiv.appendChild(address);
+        bootDiv.appendChild(city);
+        bootDiv.appendChild(telephone);
+  
+  
+        container.appendChild(bootDiv);
+       
+        }
+    })
+        }else {
+      const errorMessage = document.createElement('marquee');
+      errorMessage.textContent = `it's not working`;
+      app.appendChild(errorMessage);
+    }
+
+}
+
+    
+  request.send();
+  
+}
+  
+
